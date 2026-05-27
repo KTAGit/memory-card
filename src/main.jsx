8042db448b0f45ac7a0113d9acf36478860b29e5
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-05-27 17:10:39 -0400

    Add Score component with current and all-time best score displays

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Navbar } from "./navbar.jsx";
+import { Score } from "./Score.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
+    <Score />
     <App />
   </StrictMode>,
 );
